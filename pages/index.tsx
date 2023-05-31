d1da731c1126c4d5ed544e61eee00e6470e55939
Natalia <natnikiforuk@gmail.com>
2023-05-31 19:50:28 +0200

    Components and utils created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import Head from "next/head";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Aside from "../components/Aside";
+import Footer from "../components/Footer";
+import Favicon from "../utils/Favicon";
 
 export default function Home() {
 	return (
-		<Head>
-			<link
-				rel="icon"
-				href="data:image/svg+xml;charset=UTF-8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgNTEyIDUxMiI+PCEtLSEgRm9udCBBd2Vzb21lIEZyZWUgNi40LjAgYnkgQGZvbnRhd2Vzb21lIC0gaHR0cHM6Ly9mb250YXdlc29tZS5jb20gTGljZW5zZSAtIGh0dHBzOi8vZm9udGF3ZXNvbWUuY29tL2xpY2Vuc2UgKENvbW1lcmNpYWwgTGljZW5zZSkgQ29weXJpZ2h0IDIwMjMgRm9udGljb25zLCBJbmMuIC0tPjxwYXRoIGQ9Ik00OTkuMSA2LjNjOC4xIDYgMTIuOSAxNS42IDEyLjkgMjUuN3Y3MlYzNjhjMCA0NC4yLTQzIDgwLTk2IDgwcy05Ni0zNS44LTk2LTgwczQzLTgwIDk2LTgwYzExLjIgMCAyMiAxLjYgMzIgNC42VjE0N0wxOTIgMjIzLjhWNDMyYzAgNDQuMi00MyA4MC05NiA4MHMtOTYtMzUuOC05Ni04MHM0My04MCA5Ni04MGMxMS4yIDAgMjIgMS42IDMyIDQuNlYyMDAgMTI4YzAtMTQuMSA5LjMtMjYuNiAyMi44LTMwLjdsMzIwLTk2YzkuNy0yLjkgMjAuMi0xLjEgMjguMyA1eiIvPjwvc3ZnPg=="
-			/>
-			<meta
-				name="description"
-				content="Write and transpose your piano notes very easy"
-			/>
-			<title>myMelody</title>
-		</Head>
+		<div className="home">
+			<Head>
+				<Favicon />
+					<meta
+						name="description"
+						content="Write and transpose your piano notes very easy"
+					/>
+					<title>myMelody</title>
+			</Head>
+			<Header />
+			<Main />
+			<Aside />
+			<Footer />
+		</div>
 	);
 }
---
+++ utils/Favicon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Favicon() {
+	return (
+		<>
+			<link
+				rel="icon"
+				href="data:image/svg+xml;charset=UTF-8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgNTEyIDUxMiI+PCEtLSEgRm9udCBBd2Vzb21lIEZyZWUgNi40LjAgYnkgQGZvbnRhd2Vzb21lIC0gaHR0cHM6Ly9mb250YXdlc29tZS5jb20gTGljZW5zZSAtIGh0dHBzOi8vZm9udGF3ZXNvbWUuY29tL2xpY2Vuc2UgKENvbW1lcmNpYWwgTGljZW5zZSkgQ29weXJpZ2h0IDIwMjMgRm9udGljb25zLCBJbmMuIC0tPjxwYXRoIGQ9Ik00OTkuMSA2LjNjOC4xIDYgMTIuOSAxNS42IDEyLjkgMjUuN3Y3MlYzNjhjMCA0NC4yLTQzIDgwLTk2IDgwcy05Ni0zNS44LTk2LTgwczQzLTgwIDk2LTgwYzExLjIgMCAyMiAxLjYgMzIgNC42VjE0N0wxOTIgMjIzLjhWNDMyYzAgNDQuMi00MyA4MC05NiA4MHMtOTYtMzUuOC05Ni04MHM0My04MCA5Ni04MGMxMS4yIDAgMjIgMS42IDMyIDQuNlYyMDAgMTI4YzAtMTQuMSA5LjMtMjYuNiAyMi44LTMwLjdsMzIwLTk2YzkuNy0yLjkgMjAuMi0xLjEgMjguMyA1eiIvPjwvc3ZnPg=="
+			/>
+		</>
+	);
+}
+
+export default Favicon;
