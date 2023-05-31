89446e5aaad4cfe78a92fd2dbfff5cfcd17b9138
Natalia <natnikiforuk@gmail.com>
2023-05-31 19:31:40 +0200

    Project initialization

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+// Wraps all the pages in application
+// Keep state when navigating between pages
+// Add global styles
+
+import "../styles/globals.scss"
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
