45fe165c31b99391451b1694c653bb8a1c45e9c4
Natalia <natnikiforuk@gmail.com>
2023-06-01 15:42:47 +0200

    Title change input

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, ChangeEvent, KeyboardEvent, useEffect } from "react";
+import headerStyles from "./header.module.scss";
 
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+	const [isEditing, setIsEditing] = useState(false);
+	const [title, setTitle] = useState("Song title");
+
+	const handleClick = () => {
+		setIsEditing(true);
+	};
+
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.target.value);
+	};
+
+	const submitEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") {
+			setIsEditing(false);
+		}
+	};
+
+	const submitBackground = () => {
+		setIsEditing(false);
+	};
+
+	return (
+		<div>
+			{isEditing ? (
+				<div className={headerStyles.titleInput}>
+					<input
+						type="text"
+						value={title}
+						onChange={handleInputChange}
+						onKeyDown={(e) => submitEnter(e)}
+						onBlur={() => submitBackground()}
+					/>
+				</div>
+			) : (
+				<h1 className={headerStyles.title} onClick={handleClick}>
+					{title}
+				</h1>
+			)}
+			<h2 className={headerStyles.author}>The author</h2>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
